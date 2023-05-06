cfb922889e2f8aed774c7fe86d9ca6e20a5016ba
somya21 <71588326+somya21@users.noreply.github.com>
2023-05-06 22:29:06 +0530

    Create App.test.js

---
+++ src/App.test.js
@@ -0,0 +1,7 @@
+import { render } from '@testing-library/react';
+import App from './App';
+
+test('renders App component', () => {
+  const { container } = render(<App />);
+  expect(container.firstChild).toMatchSnapshot();
+});
