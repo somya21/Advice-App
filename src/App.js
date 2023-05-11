c7f18d4c3261aafc5358e1317703f66cb1cb5e4a
somya21 <71588326+somya21@users.noreply.github.com>
2023-05-12 00:48:28 +0530

    App.js

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ class App extends React.Component {
                 <div className = "card">
                     <h1 className = "heading">{advice}</h1>
                     <button className="button" onClick={this.fetchAdvice}>
-                        <span>Give me an Advice!</span>
+                        <span>Give Advice!</span>
                     </button>
                 </div>
             </div>
